12ff2ff515b452a8db2b2e3c3f124812444f9311
chenchristian <98064500+chenchristian@users.noreply.github.com>
2025-05-30 18:24:20 -0700

    Update single_test.py
    fixed local dependencies. 
    
    instead of intaking an image, the predict function now just takes in a vector

--- single_test.py
+++ single_test.py
@@ -4,24 +4,14 @@ from ForwardProp import forwardProp
 import time
 import os
 
-def predict_single_image(image_path, label, return_probs=False):
-
-    img = Image.open(image_path).convert('L')  # Convert to grayscale
-    img = img.resize((28, 28))  # Resize to 28x28 for MNIST
-    
-    # Convert to numpy array and normalize
-    img_array = np.array(img)
-    img_array = (img_array > 128).astype(int)  # Binarize like in your training data
-    
-    
-    # Reshape to match your model's input format
-    x = img_array.reshape(-1, 1)  # (784, 1)
+def predict_single_image(x, label, return_probs=False):
 
     
-
+    BASE_DIR = os.path.dirname(__file__)
+    weights_path = os.path.join(BASE_DIR, "fine_tuned_weights.npz")
 
     # Load trained weights
-    weights = np.load("/Users/christianchen/VSCode_Python/Stat21/fine_tuned_weights.npz")
+    weights = np.load(weights_path)
     W1 = weights["W1"]
     B1 = weights["B1"]
     W2 = weights["W2"]
